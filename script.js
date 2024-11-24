34485c7aca2b57fb325068cfbe3e37bf429bc8b8
Daksh-Singh5 <dakshsingh6612@gmail.com>
2024-11-24 19:11:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var number =document.getElementById("number").innerText;
+
+
+function increase(){
+    document.getElementById("number").innerHTML=number+1
+    
+   }
+
+   function reset(){
+    document.getElementById("number").innerHTML=0
+   }
